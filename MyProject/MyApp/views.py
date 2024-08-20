66e228da9703c61a727edccc9b488625e498b56e
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-08-20 20:57:07 +0500

    views.py

--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from .models import Task
 
@@ -7,3 +8,9 @@ def task_list(request):
     tasks = Task.objects.all()
     task_data = [{"id": task.id, "title": task.title, "description": task.description, "is_completed": task.is_completed} for task in tasks]
     return JsonResponse(task_data, safe=False)
+
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task_data = {"id": task.id, "title": task.title, "description": task.description, "is_completed": task.is_completed}
+    return JsonResponse(task_data)
